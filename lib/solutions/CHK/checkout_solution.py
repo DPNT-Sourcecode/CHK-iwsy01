1eb4e4bab4ebf4a5484a5683804fab7752aea12f
sbx_user1051 <sbx_user1051@169.254.219.117>
2023-09-10 10:26:28 +0000

    Sun Sep 10 10:26:28 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,12 +17,20 @@ def calculate_prices(skus: List[str], prices: Dict[str, int], total: int) -> int
 def checkout(skus: str) -> int:
     # apply offers first - then calculate prices
     remove_other_product_offers = [
-        ("E", (2, "B"))
+        ("E", (2, "B")),
+        ("N", (3, "M")),
+        ("R", (3, "Q")),
     ]
     multiproduct_offers = [
         ("A", {5: 200, 3: 130}),
         ("B", {2: 45}),
-        ("F", {3: 20})
+        ("F", {3: 20}),
+        ("H", {10: 80, 5: 45}),
+        ("K", {2: 150}),
+        ("P", {5: 200}),
+        ("Q", {3: 80}),
+        ("U", {4: 120}),
+        ("V", {3: 130, 2: 90}),
     ]
     skus = list(skus)
     total = 0
@@ -46,9 +54,30 @@ def checkout(skus: str) -> int:
         "D": 15,
         "E": 40,
         "F": 10,
+        "G": 20,
+        "H": 10,
+        "I": 35,
+        "J": 60,
+        "K": 80,
+        "L": 90,
+        "M": 15,
+        "N": 40,
+        "O": 10,
+        "P": 50,
+        "Q": 30,
+        "R": 50,
+        "S": 30,
+        "T": 20,
+        "U": 40,
+        "V": 50,
+        "W": 20,
+        "X": 90,
+        "Y": 10,
+        "Z": 50,
     }
 
     total = calculate_prices(skus=skus, prices=prices, total=total)
     return total
+
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,7 +6,7 @@ class TestCheckout():
         assert checkout_solution.checkout("AABACADD") == 260
 
     def test_invalid_input(self):
-        assert checkout_solution.checkout("AK") == -1
+        assert checkout_solution.checkout("A8") == -1
 
     def test_new_offers(self):
         assert checkout_solution.checkout("AAAAABBCDDEEE") == 400
@@ -16,3 +16,8 @@ class TestCheckout():
 
     def test_f_offer(self):
         assert checkout_solution.checkout("AAAAABBCDDEEEFFFF") == 430
+
+    def test_alphabet_offers(self):
+        assert checkout_solution.checkout(
+            "AAAAABBBCCDEFFGHIIJKLMMNOPPQRSTTTUVWWXYZ") == 1350
+

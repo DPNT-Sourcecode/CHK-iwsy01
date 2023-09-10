7491bd7985bf5aec794a9d2835359737cfddc426
sbx_user1051 <sbx_user1051@169.254.219.117>
2023-09-10 10:10:18 +0000

    Sun Sep 10 10:10:18 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from typing import Dict, List
-from .offer_functions import offer_A, offer_B, offer_F, offer_E
+from .offer_functions import offer_E, multiproduct_offer
 
 def calculate_prices(skus: List[str], prices: Dict[str, int], total: int) -> int:
     counts = Counter(skus)
@@ -11,20 +11,27 @@ def calculate_prices(skus: List[str], prices: Dict[str, int], total: int) -> int
             return -1
     return total
 
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     # apply offers first - then calculate prices
     offers = [
         offer_E,
-        offer_A,
-        offer_B,
-        offer_F,
+    ]
+    multiproduct_offers = [
+        ("A", {5: 200, 3: 130}),
+        ("B", {2: 45}),
+        ("F", {3: 20})
     ]
     skus = list(skus)
     total = 0
     for offer in offers:
         skus, total = offer(skus, total)
+
+    for product, price_map in multiproduct_offers:
+        skus, total = multiproduct_offer(skus=skus, total=total, product=product,
+                                         offer=price_map)
 
     # iterate over skus table the cost return the cost. return -1 for invalid input.
     prices = {
--- lib/solutions/CHK/offer_functions.py
+++ lib/solutions/CHK/offer_functions.py
@@ -29,44 +29,7 @@ def multiproduct_offer(
     # count ends up as the remainder after all offers applied
     for i in range(init_count - count):
         try:
-        skus.remove()
-
-
-def offer_A(skus: List[str], total: int) -> Tuple[List[str], int]:
-    count_a = skus.count("A")
-    offer_5_num = count_a // 5
-    offer_3_num = count_a % 5 // 3
-    remaining = count_a % 5 % 3
-    total += offer_5_num * 200
-    total += offer_3_num * 130
-    to_remove = count_a - remaining
-    for i in range(to_remove):
-        try:
-            skus.remove("A")
-        except ValueError:
-            break
-    return skus, total
-
-
-def offer_B(skus: List[str], total: int) -> Tuple[List[str], int]:
-    count_b = skus.count("B")
-    total += (count_b // 2) * 45
-    to_remove = count_b - (count_b % 2)
-    for i in range(to_remove):
-        try:
-            skus.remove("B")
-        except ValueError:
-            break
-    return skus, total
-
-
-def offer_F(skus: List[str], total: int) -> Tuple[List[str], int]:
-    count = skus.count("F")
-    total += (count // 3) * 20  # 20 is 2xF cost
-    to_remove = count - (count % 3)
-    for i in range(to_remove):
-        try:
-            skus.remove("F")
+            skus.remove(product)
         except ValueError:
             break
     return skus, total

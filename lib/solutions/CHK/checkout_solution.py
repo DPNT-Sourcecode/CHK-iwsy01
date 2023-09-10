f17657dbdfc0b54bee50e773f435b3053a3a0d6b
sbx_user1051 <sbx_user1051@169.254.219.117>
2023-09-10 08:50:32 +0000

    Sun Sep 10 08:50:32 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -10,8 +11,21 @@ def checkout(skus):
         "C": 20,
         "D": 15,
     }
-    counts = Counter()
-    
+    total = 0
+    counts = Counter(skus)
+    for item, count in counts.items():
+        if item == "A":
+            total += (count // 3) * 130
+            total += (count % 3) * prices[item]
+        elif item == "B":
+            total += (count // 2) * 45
+            total += (count % 2) * prices[item]
+        else:
+            try:
+                total += count * prices[item]
+            except KeyError:
+                return -1
+    return total
 
-    pass
+
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,8 +3,9 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        assert checkout_solution.checkout("AABACADD") == 100
+        assert checkout_solution.checkout("AABACADD") == 260
 
     def test_invalid_input(self):
         assert checkout_solution.checkout("AK") == -1
+
 
